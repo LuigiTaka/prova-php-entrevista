f9690fa4060f05d7ad034134df6f0c23e2898fa2
Luigi <luigimendeszanchett@gmail.com>
2024-01-22 20:34:19 -0300

    feat: Método para retornar as cores por id de usuário.

--- src/Models/CoresModel.php
+++ src/Models/CoresModel.php
@@ -5,4 +5,16 @@ namespace TestePratico\Models;
 class CoresModel extends BaseModel
 {
     protected string $table = "colors";
+
+
+    public function getCoresByUserId(string $id): bool|array
+    {
+        $query = <<<SQL
+SELECT C.* FROM user_colors as UC LEFT JOIN colors as C ON C.id = UC.color_id WHERE UC.user_id = ?
+SQL;
+        $conn = $this->connection->getConnection();
+        $stmt = $conn->prepare($query);
+        $stmt->execute([$id]);
+        return $stmt->fetchAll();
+    }
 }
