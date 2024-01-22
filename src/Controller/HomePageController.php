5d4c6431c7758566616f9f775e89b0b23769c5a3
Luigi <luigimendeszanchett@gmail.com>
2024-01-22 15:54:34 -0300

    refactor: Utilizia Template para renderizar a página, remove HTML do Controller

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,19 +2,16 @@
 
 namespace TestePratico\Controller;
 
+use TestePratico\Template;
+
 class HomePageController
 {
 
     public static function get(\TestePratico\Request $param): \TestePratico\Response
     {
 
-        $page =  <<<HTML
-    <h1>Teste prático de PHP.</h1>
-    <p>Um CRUD para gerir cadastros de usuários, permitindo atrelar uma ou mais cores a um usuário. O sistema deve ser feito sem frameworks e sem dependências externas.</p>
-    <a href="/usuarios"> Gerenciador de Usuários </a>
-HTML;
-
-        return \TestePratico\Response::response()->setStatus(200)->setBody( $page );
+        $page = new Template("home");
+        return \TestePratico\Response::response()->setStatus(200)->setBody( $page->render() );
 
     }
 }
